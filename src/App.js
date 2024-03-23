e02d733251c63af51d9faa0781fe9e54a51d2861
DivyKishor <divykishor@hotmail.com>
2024-03-23 21:28:44 +0530

    react-router added

--- src/App.js
+++ src/App.js
@@ -5,32 +5,52 @@ import StarRating from './components/StarRating';
 import LoadMoreData from './components/LoadMoreData';
 import QrCode from './components/QrCode';
 import IncrementCounter from './components/IncrementCounter';
-// import { useEffect } from 'react';
+import Header from './components/Header';
+import Login from './components/Login';
+import { Routes , Route} from 'react-router-dom';
+import NotFound from './components/NotFound';
+
 
 
 function App() {
-  
+
   return (
-    <div className="App">
-      <IncrementCounter/>
-      <hr/>
-      <div>Accordion</div>
-      <Accordion />
-      <br/>
-      <br/>
-      <hr/>
-      <div>Star Rating</div>
-      <StarRating noOfStars={10}/>
-      <hr/>
-      <QrCode value="Brave" />
-      <p>&nbsp;</p>
-      <hr/>
-      {/* load more product */}
-      <LoadMoreData />
-      <hr/>
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/counter" element={<IncrementCounter/>} />
+      <Route path="/accordion" element={<Accordion/>} />
+      <Route path="/stars" element={<StarRating noOfStars={10}/>} />
+      <Route path="/qr" element={<QrCode value="Brave"/>} />
+      <Route path="/loadmore" element={<LoadMoreData/>} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
+    </>
+  )
+  
+  // return (
+  //   <div className="App">
+  //     {/* <Header /> */}
+  //     <IncrementCounter/>
+  //     <hr/>
+  //     <div>Accordion</div>
+  //     <Accordion />
+  //     <br/>
+  //     <br/>
+  //     <hr/>
+  //     <div>Star Rating</div>
+  //     <StarRating noOfStars={10}/>
+  //     <hr/>
+  //     <QrCode value="Brave" />
+  //     <p>&nbsp;</p>
+  //     <hr/>
+  //     {/* load more product */}
+  //     <LoadMoreData />
+  //     <hr/>
       
-    </div>
-  );
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="flex justify-between">
+    <div className="w-36">
+        {/* <img alt="logo" className="logo" src={LOGO_URL}></img> */}
+    </div>
+    <div className="">
+        <ul className="flex items-center ">
+            <li className="m-4 p-4"><Link to="/">Home</Link></li>
+            <li className="m-4 p-4"><Link to="/accordion">Accordion</Link></li>
+            <li className="m-4 p-4"><Link to="/loadmore">Load More Item</Link></li>
+            <li className="m-4 p-4"><Link to="/qr">QR Code</Link></li>
+            <li className="m-4 p-4"><Link to="/stars">Star Rating</Link></li>
+            <li className="m-4 p-4"><Link to="/counter">Counter</Link></li>
+            <li className="m-4 p-4">Log in</li>
+        </ul>
+    </div>
+</div>
+  )
+}
+
+export default Header
